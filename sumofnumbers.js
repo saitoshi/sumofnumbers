524c3080268e1da0d6b7bfd5263900c9799677be
Shun <47809066+saitoshi@users.noreply.github.com>
2020-02-09 18:23:03 -1000

    finished exercise.

---
+++ sumofnumbers.js
@@ -0,0 +1,36 @@
+const testNums = [1, 2, 3, 4];
+
+function sumFor(nums) {
+  let total = 0;
+  for (const num of nums) total += num;
+
+  return total;
+}
+
+function sumWhile(nums) {
+  let total = 0;
+  let i = 0;
+  while ( i < nums.length) {
+    total += nums[i];
+    i++;
+  }
+  return total;
+}
+
+function sumRecursion(nums) {
+  if (nums.length === 0) {
+    return 0;
+  } else {
+    return nums[0] + sumRecursion(nums.slice(1, nums.length ));
+  }
+}
+
+function sumTheSimpleWay(nums) {
+  return _.reduce(nums, function (memo, num) {
+    return memo + num;
+  }, 0);
+}
+console.log(sumFor(testNums));
+console.log(sumWhile(testNums));
+console.log(sumRecursion(testNums));
+console.log(sumTheSimpleWay(testNums));
